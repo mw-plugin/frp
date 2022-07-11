62e179c7ca9ff3d2a71e1f0b20126b636701ff81
DDSRem <1448139087@qq.com>
2022-07-11 11:40:27 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,14 +6,13 @@ export PATH
 curPath=`pwd`
 rootPath=$(dirname "$curPath")
 
-wget -O /tmp/mw_plugin.zip https://github.com/mw-plugin/simple-plugin/archive/refs/heads/main.zip
+
+wget -O /tmp/mw_plugin.zip https://github.com/mw-plugin/frpc/archive/refs/heads/main.zip
 cd /tmp && unzip /tmp/mw_plugin.zip 
 
-
 mkdir -p /www/server/mdserver-web/plugins/frpc
-cp -rf  /tmp/frp_0.44.0_linux_amd64/frpc /www/server/mdserver-web/plugins/frpc/
-cp -rf  /tmp/frp_0.44.0_linux_amd64/frpc.ini /www/server/mdserver-web/plugins/frpc/
+cp -rf  /tmp/frpc-main/* /www/server/mdserver-web/plugins/frpc
 
 
-rm -rf /tmp/frp_0.44.0_linux_amd64
-rm -rf /tmp/frp.tar.gz
+rm -rf /tmp/mw_plugin.zip
+rm -rf /tmp/frpc-main
