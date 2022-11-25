9efff5142c47a30ee54fe3c01ecfe50ec25d05eb
midoks <midoks@163.com>
2022-11-25 20:06:58 +0800

    up

---
+++ js/frp.js
@@ -0,0 +1,6 @@
+function readme(){
+    var readme = '<ul class="help-info-text c7">';
+    readme += '<li>请注意端口需要在防火墙自己开启或关闭。</li>';
+    readme += '</ul>';
+    $('.soft-man-con').html(readme);
+}
